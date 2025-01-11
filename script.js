e9aea518220946c8334e19132496ca2723d30983
sdmv <74918482+sdmv92@users.noreply.github.com>
2025-01-11 13:28:08 +0530

    Etch-a-Sketch
    Make a game similar to Etch-a-sktech with mouse over color change

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector("#container")
+const button = document.querySelector("#button")
+button.addEventListener("click", function(){
+    let numberOfBoxes = prompt("Enter squared from 0-100")
+    if (numberOfBoxes<=100 && numberOfBoxes >=0){
+        let gridBoxes = numberOfBoxes * numberOfBoxes
+    for(counter = 0 ; counter < gridBoxes; counter++){
+        const box = document.createElement("div")
+        box.style.width = (600/numberOfBoxes) + "px";
+        box.style.height = (600/numberOfBoxes) + "px";
+        container.appendChild(box)
+        box.addEventListener("mouseover",()=> { box.style.background = randomColor()})
+    }
+    }else 
+    alert("Invalid number")
+})
+
+function randomColor(){
+    let x=Math.floor(Math.random()*255);
+    let y=Math.floor(Math.random()*255);
+    let z=Math.floor(Math.random()*255);
+    let color= "rgb("+x+","+y+","+z+")";
+    return color;
+}
+    
+
+
